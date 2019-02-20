779739cc065bd7e522e73780ad829c02bb571d51
CameronForrest <44378657+CameronForrest@users.noreply.github.com>
2019-02-20 08:35:36 -0600

    re-done Javascript
    completely scratched the original JavaScript to restructure this one

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,49 +1,68 @@
 //Declaring Variables
 var wins= 0;
 var losses= 0;
-var scoreHolder =[]
+var scoreHolder = 0;
+var winningScore;
+var blueValue;
+var greyValue;
+var orangeValue;
+var purpleValue;
 
 
-//Create random guesses for point you need to guess
-var winningScore = [Math.floor(Math.random() *100)];
-console.log(winningScore)
-
-function gemValue(min, max) {
-    return Math.floor(Math.random() * (max-min +1)) +min;
+//Random number user needs to reach to win
+function createWinningScore() {
+    winningScore = Math.floor(Math.random() * 82) + 19;
+    $("#points").text(winningScore);
+    console.log(winningScore);
+    
 }
-console.log(gemValue(2,12))
 
-$(document).ready(function() {
-//Create random guess for gem between 2-12
+//Random Values generated for gems
+function creategemValues() {
+    oneValue = Math.floor(Math.random() * 12 + 2);
+    twoValue = Math.floor(Math.random() * 12 + 2);
+    threeValue = Math.floor(Math.random() * 12 + 2);
+    fourValue = Math.floor(Math.random() * 12 + 2);
+}
 
+//On click events for gems
+$("#blue").on("click", function () {
+    console.log(oneValue); 
+    scoreHolder += oneValue;
 
-//On click events for images
-$("#blue").on("click",function() {
-    gemValue(2,12)
-    console.log(gemValue(2,12));
+    $("#yourScore").text(scoreHolder);
 });
 
-$("#grey").on("click",function() {
-    gemValue(2,12);
-    console.log(gemValue(2,12));
+$("#grey").on("click", function () {
+    console.log(twoValue); 
+    scoreHolder += twoValue;
+
+    $("#yourScore").text(scoreHolder);
 });
 
-$("#orange").on("click",function() {
-    gemValue(2,12);
-    console.log(gemValue(2,12));
+$("#orange").on("click", function () {
+    console.log(threeValue); 
+    scoreHolder += threeValue;
+
+    $("#yourScore").text(scoreHolder);
 });
 
-$("#purple").on("click",function() {
-    gemValue(2,12);
-    console.log(gemValue(2,12));
+$("#purple").on("click", function () {
+    console.log(fourValue); 
+    scoreHolder += fourValue;
+
+    $("#yourScore").text(scoreHolder);
 });
 
-var bluePoints = "";
-var greyPoints = "";
-var orangePoints ="";
-var purplePoints ="";
+//Reset function
+function reset() {
+    scoreHolder = 0;
+    $("yourScore").text(scoreHolder);
+    
+    createWinningScore();
+    creategemValues();
 
-//Write cpu random score
-$("#points").text(winningScore);
+   
+}
 
-});
+reset();
