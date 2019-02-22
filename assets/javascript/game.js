51ce6420c10013c7d774b4a7789f6898485b1f45
bam forrest <cameron.forr@gmail.com>
2019-02-21 22:38:04 -0600

    final push

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,10 +3,7 @@ var wins= 0;
 var losses= 0;
 var scoreHolder = 0;
 var winningScore;
-var blueValue;
-var greyValue;
-var orangeValue;
-var purpleValue;
+ 
 
 
 //Random number user needs to reach to win
@@ -26,43 +23,66 @@ function creategemValues() {
 }
 
 //On click events for gems
-$("#blue").on("click", function () {
-    console.log(oneValue); 
+$("#blue").on("click", function () { 
     scoreHolder += oneValue;
-
+    winsLosses();
     $("#yourScore").text(scoreHolder);
+    
 });
 
-$("#grey").on("click", function () {
-    console.log(twoValue); 
+$("#grey").on("click", function () { 
     scoreHolder += twoValue;
-
+    winsLosses();
     $("#yourScore").text(scoreHolder);
+    
 });
 
 $("#orange").on("click", function () {
-    console.log(threeValue); 
     scoreHolder += threeValue;
-
+    winsLosses();
     $("#yourScore").text(scoreHolder);
+    
 });
 
-$("#purple").on("click", function () {
-    console.log(fourValue); 
+$("#purple").on("click", function () { 
     scoreHolder += fourValue;
-
+    winsLosses();
     $("#yourScore").text(scoreHolder);
+    
 });
 
+//determine win and loss
+function winsLosses() {
+    if (scoreHolder === winningScore) {
+        wins++;
+        $("#win").text(wins);
+        reset();
+        
+    }
+
+    else if (scoreHolder > winningScore){
+        losses++;
+        $("#loss").text(losses);
+        reset();
+    }
+
+    
+}
 //Reset function
 function reset() {
     scoreHolder = 0;
-    $("yourScore").text(scoreHolder);
+    $("#yourScore").text(scoreHolder);
     
     createWinningScore();
     creategemValues();
 
-   
+    $("#win").text(wins);
+    $("#loss").text(losses);
+
+    wins =0;
+    losses=0;
+
+    
 }
 
 reset();
